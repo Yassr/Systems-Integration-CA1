17a8e144aa7c6ab51701a3e65fdccff662935dcb
Yassr Shaar <c14328571@mydit.ie>
2017-10-25 11:23:23 +0100

    C attempt

---
+++ cca1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+
+main{
+	x = 1;
+	String cmd;
+	while (x = 1){
+		
+		switch(cmd){
+			case (pwd || pw):
+			
+		}
+		
+		
+		
+		
+		
+	}
+
+
+}
+
+if[cmd = ifc]{
+	ifconfig | grep "eth0" -A 8
+else
+	ifconfig | grep "INPUT:4" -A 8
+}
